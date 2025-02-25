87e29eed506e39d289564bb08a84c076830fd405
Eluwa Emmanuel <emmaeluwa2021@gmail.com>
2025-02-26 00:09:26 +0100

    created budget API's

---
+++ routes/BudgetRoute.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const {
+	getBudgets,
+	getExpensesByCategory,
+	createBudget,
+} = require("../controllers/BudgetController");
+const authMiddleware = require("../middlewares/authMiddlewares");
+const router = express.Router();
+
+router.post("/createBudget", authMiddleware, createBudget);
+router.get("/getBudgets", authMiddleware, getBudgets);
+router.get(
+	"/getBudgetCategory/expenses",
+	authMiddleware,
+	getExpensesByCategory
+);
+module.exports = router;
